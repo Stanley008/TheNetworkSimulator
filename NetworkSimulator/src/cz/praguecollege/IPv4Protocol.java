eeba0461fbde14689943af7ccd588babae78be7e
ilyassakhanov <ilyas.sakhanov@gmail.com>
2021-01-17 17:28:08 +0100

    Rename address to source address

--- NetworkSimulator/src/cz/praguecollege/IPv4Protocol.java
+++ NetworkSimulator/src/cz/praguecollege/IPv4Protocol.java
@@ -5,9 +5,9 @@ public class IPv4Protocol implements  NetProtocol {
     public int getProtocolID() {
         return 4;
     }
-    IPv4Address address;
-    IPv4Interface ipv4interface = new IPv4Interface(address);
-    public void setAddress(IPv4Address address) {
-        this.address = address;
+    IPv4Address sourceAddress;
+    IPv4Interface ipv4interface = new IPv4Interface(sourceAddress);
+    public void setSourceAddress(IPv4Address address) {
+        this.sourceAddress = address;
     }
 }
