ae127eba243a0fb0b7f2ace1f9f825aae9d00035
Tocrak <virgiliustsirdea@yandex.ru>
2021-01-26 12:54:42 +0100

    Add reference to the event receiver in the event object

--- NetworkSimulator/src/cz/praguecollege/Event.java
+++ NetworkSimulator/src/cz/praguecollege/Event.java
@@ -1,7 +1,5 @@
 package cz.praguecollege;
 
-import java.util.Date;
-
 enum EventType {
     CHECK_UPPER_QUEUE,
     CHECK_LOWER_QUEUE
@@ -10,14 +8,16 @@ enum EventType {
 public class Event {
     private final EventType type;
     private final Long timestamp;
+    private final EventReciever eventReciever;
 
-    public static Event createEvent(EventType type, Long timestamp){
-        return new Event(type, timestamp);
+    public static Event createEvent(EventType type, Long timestamp, EventReciever eventReciever){
+        return new Event(type, timestamp, eventReciever);
     }
 
-    private Event(EventType type, Long timestamp){
+    private Event(EventType type, Long timestamp,EventReciever eventReciever){
         this.type = type;
         this.timestamp = timestamp;
+        this.eventReciever = eventReciever;
     }
 
     public EventType getType(){
@@ -27,4 +27,6 @@ public class Event {
     public Long getTime(){
         return timestamp;
     }
+
+    public EventReciever getEventReciever() {return eventReciever;}
 }
